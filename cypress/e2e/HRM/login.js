c1c4938eba02e6df8b1c8704f09d0133582ee7a9
AliRaza <ali.raza@etaplus.energy>
2023-09-15 20:20:22 +0200

    admin

--- cypress/e2e/HRM/login.js
+++ cypress/e2e/HRM/login.js
@@ -10,5 +10,26 @@ When("provide a valide user name and password",()=>{
 })
 
 Then("click on submit button",()=>{
-    cy.get('#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
+    cy.get('#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click().wait(3000)
+})
+And("click on admin",()=>{
+    cy.get('#app > div.oxd-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(1) > a').click().wait(3000)
+})
+
+And("type on text field",()=>{
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input').type('Aaliyah.Haq')
+   
+    
+}) 
+And("click on Role",()=>{
+    
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(2) > div > div:nth-child(2) > div > div').click()
+    
+    
+})
+And("click on Ess",()=>{
+    
+   
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div.oxd-select-text-input').click()
+    
 })
--- cypress/e2e/admin.feature
+++ cypress/e2e/admin.feature
@@ -4,9 +4,3 @@ Scenario: Eta+ page
 Given open init login page
 When provide a valide user name and password
 Then click on submit button 
-And click on admin
-And type on text field 
-And click on Role
-And click on Ess
-
-
